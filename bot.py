29f9f14db36bdc53978bb98a83ca857e347c80b0
Артём <129595378+ArtemShevchenko2023@users.noreply.github.com>
2024-06-05 17:37:16 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,15 @@ import telebot
 from conf import *
 from gen import *
 bot = telebot.TeleBot(token)
-@bot.message_handler(commands=['help', 'start', 'gen'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "hello")
-    # prompt = message.text
-    # api = Text2ImageAPI(fus, ap, secret)
-    # api.conv(prompt)
-    # img = open('C:\Users\user\Desktop\AtomAll\LVL3\M6L1\decoded.jpg', 'rb')
-    # bot.send_photo(message.chat.id, img)
+    bot.reply_to(message, "Привет! Для помощи введи /help")
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Вот список моих команд:\n/start - приветствие\n/help - все команды\nНапиши любое сообщение с запросом что хочешь увидеть в изображении!")
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
+    bot.reply_to(message, "Генерирую картинку...")
     prompt = message.text
     api = Text2ImageAPI(fus, ap, secret)
     api.conv(prompt)
